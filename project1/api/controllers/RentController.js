52912e839e462647542f43ce06491aec3f44694c
singsing215 <felvex@163.com>
2019-10-08 11:40:39 +0800

    delete

--- project1/api/controllers/RentController.js
+++ project1/api/controllers/RentController.js
@@ -48,5 +48,51 @@ index: async function (req, res) {
 
     },
 
+
+// action - delete 
+delete: async function (req, res) {
+
+    if (req.method == "GET") return res.forbidden();
+
+    var models = await Rent.destroy(req.params.id).fetch();
+
+    if (models.length == 0) return res.notFound();
+
+    return res.ok("Rent Deleted.");
+
+},
+
+// action - update
+update: async function (req, res) {
+
+    if (req.method == "GET") {
+
+        var model = await Rent.findOne(req.params.id);
+
+        if (!model) return res.notFound();
+
+        return res.view('rent/update', { rent: model });
+
+    } else {
+
+        if (!req.body.Rent)
+            return res.badRequest("Form-data not received.");
+
+        var models = await Rent.update(req.params.id).set({
+            title: req.body.Rent.title,
+            url: req.body.Rent.url,
+            bedroom: req.body.Rent.bedroom,
+            area: req.body.Rent.area,
+            tenant: req.body.Rent.tenant,
+            rent: req.body.Rent.rent
+
+        }).fetch();
+
+        if (models.length == 0) return res.notFound();
+
+        return res.ok("Record updated");
+
+    }
+
 };
 
