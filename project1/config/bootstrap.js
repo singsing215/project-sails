ed52f7d262b9159a5d3de235684e76d12107e975
singsing215 <felvex@163.com>
2019-10-08 12:06:49 +0800

    initial testing data

--- project1/config/bootstrap.js
+++ project1/config/bootstrap.js
@@ -26,5 +26,18 @@ module.exports.bootstrap = async function() {
   //   // etc.
   // ]);
   // ```
+  if (await Rent.count() > 0) {
+    return done();
+}
+
+await Rent.createEach([
+    { title: "半山自住品味靚裝-三房兩廁-中高層開揚景觀", url: "https://i1.28hse.com/2019/09/938689_2019094721.jpg", bedroom:3, area:899, tenant:5, rent:43000 },
+    { title: "銅鑼灣2房2廳。即租即住。優質罕盤。", url:"https://i1.28hse.com/2019/10/951089_2019104698.jpg", bedroom:2, area:700, tenant:3, rent:26500 },
+    { title: "形品星寓。3分鐘到地鐵站", url:"https://i1.28hse.com/2019/09/930790_2019095031.jpg", bedroom:1, area:390, tenant:2, rent:19000 },
+    { title: "中上環服務式住宅 2分鐘到地鐵", url:" https://i1.28hse.com/2019/10/955789_2019101877.jpg", bedroom:1, area:605, tenant:2, rent:35000 }
+    // etc.
+]);
+
+
 
 };
