f6ed4223d18e3bfe2579825a771803963026d2d1
guofusheng <guofusheng@youcash.com>
2021-04-29 22:25:05 +0800

    feat: formatter

--- sails-app/api/controllers/RentController.js
+++ sails-app/api/controllers/RentController.js
@@ -6,278 +6,322 @@
  */
 
 module.exports = {
-    // action - create
-    create: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        if (req.method == "GET") return res.view("rent/create");
-        if (!req.body.Rent) return res.badRequest("Form-data not received.");
-        await Rent.create(req.body.Rent);
-        return res.ok("Successfully created!");
-    },
+  // action - create
+  create: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    if (req.method == "GET") return res.view("rent/create");
+    if (!req.body.Rent) return res.badRequest("Form-data not received.");
+    await Rent.create(req.body.Rent);
+    return res.ok("Successfully created!");
+  },
 
-    // json function
-    json: async function(req, res) {
-        var rents = await Rent.find();
-        return res.json(rents);
-    },
+  // json function
+  json: async function (req, res) {
+    var rents = await Rent.find();
+    return res.json(rents);
+  },
 
-    // action - index
-    index: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        var models = await Rent.find();
-        return res.view("rent/index", { rents: models });
-    },
+  // action - index
+  index: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    var models = await Rent.find();
+    return res.view("rent/index", {
+      rents: models
+    });
+  },
 
-    // action - view
-    view: async function(req, res) {
-        var model = await Rent.findOne(req.params.id);
-        if (!model) return res.notFound();
-        return res.view("rent/view", { rent: model });
-    },
+  // action - view
+  view: async function (req, res) {
+    var model = await Rent.findOne(req.params.id);
+    if (!model) return res.notFound();
+    return res.view("rent/view", {
+      rent: model
+    });
+  },
 
-    // action - delete
-    delete: async function(req, res) {
-        if (req.wantsJSON) {
-            if (req.method == "GET") return res.forbidden();
-            var models = await Rent.destroy(req.params.id).fetch();
-            if (models.length == 0) return res.notFound();
-            return res.json({ message: "Rental information deleted.", url: "/" }); // for ajax request
-        } else {
-            return res.redirect("/"); // for normal request
-        }
-    },
+  // action - delete
+  delete: async function (req, res) {
+    if (req.wantsJSON) {
+      if (req.method == "GET") return res.forbidden();
+      var models = await Rent.destroy(req.params.id).fetch();
+      if (models.length == 0) return res.notFound();
+      return res.json({
+        message: "Rental information deleted.",
+        url: "/"
+      }); // for ajax request
+    } else {
+      return res.redirect("/"); // for normal request
+    }
+  },
 
-    // action - update
-    update: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        if (req.method == "GET") {
-            var model = await Rent.findOne(req.params.id);
-            if (!model) return res.notFound();
-            return res.view("rent/update", { rent: model });
-        } else {
-            if (!req.body.Rent) return res.badRequest("Form-data not received.");
-            var models = await Rent.update(req.params.id)
-                .set({
-                    title: req.body.Rent.title,
-                    estate: req.body.Rent.estate,
-                    url: req.body.Rent.url,
-                    bedroom: req.body.Rent.bedroom,
-                    area: req.body.Rent.area,
-                    tenant: req.body.Rent.tenant,
-                    rent: req.body.Rent.rent,
-                    created: req.body.Rent.created,
-                    updated: req.body.Rent.updated,
-                })
-                .fetch();
-            if (models.length == 0) return res.notFound();
-            return res.ok("Record updated");
-        }
-    },
+  // action - update
+  update: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    if (req.method == "GET") {
+      var model = await Rent.findOne(req.params.id);
+      if (!model) return res.notFound();
+      return res.view("rent/update", {
+        rent: model
+      });
+    } else {
+      if (!req.body.Rent) return res.badRequest("Form-data not received.");
+      var models = await Rent.update(req.params.id)
+        .set({
+          title: req.body.Rent.title,
+          estate: req.body.Rent.estate,
+          url: req.body.Rent.url,
+          bedroom: req.body.Rent.bedroom,
+          area: req.body.Rent.area,
+          tenant: req.body.Rent.tenant,
+          rent: req.body.Rent.rent,
+          created: req.body.Rent.created,
+          updated: req.body.Rent.updated,
+        })
+        .fetch();
+      if (models.length == 0) return res.notFound();
+      return res.ok("Record updated");
+    }
+  },
 
-    // action - paginate + search
-    paginate: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        const qPage = Math.max(req.query.page - 1, 0) || 0;
-        const numOfItemsPerPage = 2;
-        const qEstate = req.query.estate;
-        const qmaxArea = parseInt(req.query.maxarea);
-        // var models = {};
-        // if (!isNaN(qBedroom)) models.Bedroom = qBedroom;
-        if (isNaN(qmaxArea)) {
-            var models = await Rent.find({
-                limit: numOfItemsPerPage,
-                skip: numOfItemsPerPage * qPage,
-                where: { estate: qEstate },
-                sort: "estate",
-            });
-        } else if (!isNaN(qmaxArea)) {
-            var models = await Rent.find({
-                limit: numOfItemsPerPage,
-                skip: numOfItemsPerPage * qPage,
-                where: { area: { "<=": qmaxArea } },
-                sort: "estate",
-            });
-        } else {
-            var models = await Rent.find({
-                limit: numOfItemsPerPage,
-                skip: numOfItemsPerPage * qPage,
+  // action - paginate + search
+  paginate: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    const qPage = Math.max(req.query.page - 1, 0) || 0;
+    const numOfItemsPerPage = 2;
+    const qEstate = req.query.estate;
+    const qmaxArea = parseInt(req.query.maxarea);
+    // var models = {};
+    // if (!isNaN(qBedroom)) models.Bedroom = qBedroom;
+    if (isNaN(qmaxArea)) {
+      var models = await Rent.find({
+        limit: numOfItemsPerPage,
+        skip: numOfItemsPerPage * qPage,
+        where: {
+          estate: qEstate
+        },
+        sort: "estate",
+      });
+    } else if (!isNaN(qmaxArea)) {
+      var models = await Rent.find({
+        limit: numOfItemsPerPage,
+        skip: numOfItemsPerPage * qPage,
+        where: {
+          area: {
+            "<=": qmaxArea
+          }
+        },
+        sort: "estate",
+      });
+    } else {
+      var models = await Rent.find({
+        limit: numOfItemsPerPage,
+        skip: numOfItemsPerPage * qPage,
 
-                where: { estate: qEstate, area: { "<=": qmaxArea } },
-                sort: "estate",
-            });
-        }
-        if (!models) return res.notFound();
-        var numOfPage = Math.ceil((await Rent.count()) / numOfItemsPerPage);
-        return res.view("rent/paginate", { rents: models, count: numOfPage });
-    },
+        where: {
+          estate: qEstate,
+          area: {
+            "<=": qmaxArea
+          }
+        },
+        sort: "estate",
+      });
+    }
+    if (!models) return res.notFound();
+    var numOfPage = Math.ceil((await Rent.count()) / numOfItemsPerPage);
+    return res.view("rent/paginate", {
+      rents: models,
+      count: numOfPage
+    });
+  },
 
-    jestate: async function(req, res) {
-        var rents = await Rent.find();
-        return res.json(rents);
-    },
+  jestate: async function (req, res) {
+    var rents = await Rent.find();
+    return res.json(rents);
+  },
 
-    // json - to search max bedroom
-    jpaginate: async function(req, res) {
-        const qEstate = req.query.estate;
-        const qBedroom = parseInt(req.query.bedroom);
+  // json - to search max bedroom
+  jpaginate: async function (req, res) {
+    const qEstate = req.query.estate;
+    const qBedroom = parseInt(req.query.bedroom);
 
-        var models = await Rent.find({
-            where: { estate: qEstate, bedroom: qBedroom },
-            sort: "estate",
+    var models = await Rent.find({
+      where: {
+        estate: qEstate,
+        bedroom: qBedroom
+      },
+      sort: "estate",
+    });
+    if (!isNaN(qBedroom)) models.bedroom = qBedroom; //require
+    else {
+      models.estate = qEstate;
+      models.bedroom = qBedroom;
+    }
+    if (!models) return res.notFound();
+    return res.json(models);
+  },
+
+  // json - to search min bedroom
+  jjpaginate: async function (req, res) {
+    const qEstate = req.query.estate;
+    const qmaxBedroom = parseInt(req.query.maxbedroom);
+    const qmaxArea = parseInt(req.query.maxarea);
+    const qminArea = parseInt(req.query.minarea);
+    const qmaxRent = parseInt(req.query.maxrent);
+    const qminRent = parseInt(req.query.minrent);
+    if (isNaN(qmaxArea)) {
+      var models = await Rent.find({
+        where: {
+          estate: qEstate
+        },
+        sort: "estate",
+      });
+    } else if (!isNaN(qmaxArea)) {
+      var models = await Rent.find({
+        where: {
+          bedroom: {
+            ">=": qmaxArea
+          }
+        },
+        sort: "estate",
+      });
+    } else {
+      var models = await Rent.find({
+        where: {
+          estate: qEstate,
+          bedroom: {
+            ">=": qmaxArea
+          }
+        },
+        sort: "estate",
+      });
+    }
+    if (!models) return res.notFound();
+    return res.json(models);
+  },
+
+  // action - home
+  home: async function (req, res) {
+    const numOfItemsPerPage = 4;
+    var models = await Rent.find({
+      where: {
+        property: "dummy"
+      },
+      sort: "created",
+      limit: numOfItemsPerPage,
+    });
+    sails.log("[Session] ", req.session);
+    sails.log("[body] ", req.body);
+    return res.view("rent/home", {
+      rents: models
+    });
+  },
+
+  // action - jhome
+  jhome: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    const qPage = Math.max(req.query.page - 1, 0) || 0;
+    const numOfItemsPerPage = 4;
+    var models = await Rent.find({
+      where: {
+        property: "dummy"
+      },
+      sort: "created",
+      limit: numOfItemsPerPage,
+      skip: numOfItemsPerPage * qPage,
+    });
+    var numOfPage = Math.ceil((await Rent.count()) / numOfItemsPerPage);
+    return res.json(models);
+  },
+
+  // action - details
+  details: async function (req, res) {
+    if (!req.session.username) {
+      var visitor = "123";
+      req.session.username = visitor;
+      req.session.abc = visitor;
+    }
+    if (req.session.username == "kenny") {
+      var thatRent = await Rent.findOne(req.params.id).populate("rentby");
+      const thatRentby = await Rent.findOne(req.params.id).populate("rentby", {
+        id: req.session.userId,
+      });
+      if (thatRentby.rentby.length == 0) {
+        req.session.message = "corent";
+      }
+      if (thatRentby.rentby.length >= 1) {
+        req.session.message = "moveout";
+      }
+      if (thatRent.rentby.length >= Rent.tenant) {
+        req.session.message = "full";
+      }
+    }
+    if (req.method == "GET") {
+      var model = await Rent.findOne(req.params.id);
+      if (!model) return res.notFound();
+      var thatRent = await Rent.findOne(req.params.id).populate("rentby");
+      if (!thatRent) return res.notFound();
+      if (thatRent.rentby.length >= Rent.tenant) {
+        return res.view("rent/details", {
+          rent: model,
+          message: "full"
         });
-        if (!isNaN(qBedroom)) models.bedroom = qBedroom; //require
-        else {
-            models.estate = qEstate;
-            models.bedroom = qBedroom;
-        }
-        if (!models) return res.notFound();
-        return res.json(models);
-    },
-
-    // json - to search min bedroom
-    jjpaginate: async function(req, res) {
-        const qEstate = req.query.estate;
-        const qmaxBedroom = parseInt(req.query.maxbedroom);
-        const qmaxArea = parseInt(req.query.maxarea);
-        const qminArea = parseInt(req.query.minarea);
-        const qmaxRent = parseInt(req.query.maxrent);
-        const qminRent = parseInt(req.query.minrent);
-        if (isNaN(qmaxArea)) {
-            var models = await Rent.find({
-                where: { estate: qEstate },
-                sort: "estate",
-            });
-        } else if (!isNaN(qmaxArea)) {
-            var models = await Rent.find({
-                where: { bedroom: { ">=": qmaxArea } },
-                sort: "estate",
-            });
-        } else {
-            var models = await Rent.find({
-                where: { estate: qEstate, bedroom: { ">=": qmaxArea } },
-                sort: "estate",
-            });
-        }
-        if (!models) return res.notFound();
-        return res.json(models);
-    },
-
-    // action - home
-    home: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        const qPage = Math.max(req.query.page - 1, 0) || 0;
-        const numOfItemsPerPage = 4;
-        var models = await Rent.find({
-            where: { property: "dummy" },
-            sort: "created",
-            limit: numOfItemsPerPage,
-            skip: numOfItemsPerPage * qPage,
+      }
+      const thatRentby = await Rent.findOne(req.params.id).populate("rentby", {
+        id: req.session.userId,
+      }); //req.session.userId
+      if (thatRentby.rentby.length >= 1) {
+        return res.view("rent/details", {
+          rent: model,
+          message: "moveout"
         });
-        var numOfPage = Math.ceil((await Rent.count()) / numOfItemsPerPage);
-        sails.log("[Session] ", req.session);
-        sails.log("[body] ", req.body);
-        return res.view("rent/home", { rents: models, count: numOfPage });
-    },
-
-    // action - home
-    jhome: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        const qPage = Math.max(req.query.page - 1, 0) || 0;
-        const numOfItemsPerPage = 4;
-        var models = await Rent.find({
-            where: { property: "dummy" },
-            sort: "created",
-            limit: numOfItemsPerPage,
-            skip: numOfItemsPerPage * qPage,
+      }
+      if (thatRentby.rentby.length == 0) {
+        return res.view("rent/details", {
+          rent: model,
+          message: "corent"
         });
-        var numOfPage = Math.ceil((await Rent.count()) / numOfItemsPerPage);
-        return res.json(models);
-    },
+      }
+      sails.log("[Session] ", req.session);
+    } else {
+      if (!req.body.Rent) return res.badRequest("Form-data not received.");
+      var models = await Rent.details(req.params.id)
+        .set({
+          title: req.body.Rent.title,
+          url: req.body.Rent.url,
+          estate: req.body.Rent.estate,
+          bedroom: req.body.Rent.bedroom,
+          area: req.body.Rent.area,
+          tenant: req.body.Rent.tenant,
+          rent: req.body.Rent.rent,
+        })
+        .fetch();
+      if (models.length == 0) return res.notFound();
+    }
+  },
 
-    // action - details
-    details: async function(req, res) {
-        if (!req.session.username) {
-            var visitor = "123";
-            req.session.username = visitor;
-            req.session.abc = visitor;
-        }
-        if (req.session.username == "kenny") {
-            var thatRent = await Rent.findOne(req.params.id).populate("rentby");
-            const thatRentby = await Rent.findOne(req.params.id).populate("rentby", {
-                id: req.session.userId,
-            });
-            if (thatRentby.rentby.length == 0) {
-                req.session.message = "corent";
-            }
-            if (thatRentby.rentby.length >= 1) {
-                req.session.message = "moveout";
-            }
-            if (thatRent.rentby.length >= Rent.tenant) {
-                req.session.message = "full";
-            }
-        }
-        if (req.method == "GET") {
-            var model = await Rent.findOne(req.params.id);
-            if (!model) return res.notFound();
-            var thatRent = await Rent.findOne(req.params.id).populate("rentby");
-            if (!thatRent) return res.notFound();
-            if (thatRent.rentby.length >= Rent.tenant) {
-                return res.view("rent/details", { rent: model, message: "full" });
-            }
-            const thatRentby = await Rent.findOne(req.params.id).populate("rentby", {
-                id: req.session.userId,
-            }); //req.session.userId
-            if (thatRentby.rentby.length >= 1) {
-                return res.view("rent/details", { rent: model, message: "moveout" });
-            }
-            if (thatRentby.rentby.length == 0) {
-                return res.view("rent/details", { rent: model, message: "corent" });
-            }
-            sails.log("[Session] ", req.session);
-        } else {
-            if (!req.body.Rent) return res.badRequest("Form-data not received.");
-            var models = await Rent.details(req.params.id)
-                .set({
-                    title: req.body.Rent.title,
-                    url: req.body.Rent.url,
-                    estate: req.body.Rent.estate,
-                    bedroom: req.body.Rent.bedroom,
-                    area: req.body.Rent.area,
-                    tenant: req.body.Rent.tenant,
-                    rent: req.body.Rent.rent,
-                })
-                .fetch();
-            if (models.length == 0) return res.notFound();
-        }
-    },
-
-    populate: async function(req, res) {
-        var model = await Rent.findOne(req.params.id).populate("rentby");
-        if (!model) return res.notFound();
-        return res.json(model);
-    },
+  populate: async function (req, res) {
+    var model = await Rent.findOne(req.params.id).populate("rentby");
+    if (!model) return res.notFound();
+    return res.json(model);
+  },
 };
--- sails-app/config/routes.js
+++ sails-app/config/routes.js
@@ -11,72 +11,61 @@
 module.exports.routes = {
 
   /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
 
 
-  '/': { view: 'rent/home' },
+  '/': {
+    view: 'rent/home'
+  },
 
 
   /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
- 'GET /rent/create' : 'RentController.create',
- 'POST /rent/create' : 'RentController.create',
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
+  'GET /rent/create': 'RentController.create',
+  'POST /rent/create': 'RentController.create',
 
 
- 'GET /rent/json' : 'RentController.json',
- 'GET /rent/index' : 'RentController.index',
-'GET /rent/view/:id': 'RentController.view',
-// 'GET /rent/delete/:id': 'RentController.delete',
-// 'POST /rent/delete/:id': 'RentController.delete',
-'DELETE /rent/:id': 'RentController.delete',
-'GET /rent/update/:id': 'RentController.update',
-'POST /rent/update/:id': 'RentController.update',
+  'GET /rent/json': 'RentController.json',
+  'GET /rent/index': 'RentController.index',
+  'GET /rent/view/:id': 'RentController.view',
+  'DELETE /rent/:id': 'RentController.delete',
+  'GET /rent/update/:id': 'RentController.update',
+  'POST /rent/update/:id': 'RentController.update',
+  'GET /rent/paginate': 'RentController.paginate',
+  'GET /rent/jpaginate': 'RentController.jpaginate',
+  'GET /rent/jjpaginate': 'RentController.jjpaginate',
+  'POST /rent/paginate': 'RentController.paginate',
+  'GET /rent/details/:id': 'RentController.details',
 
-'GET /rent/paginate': 'RentController.paginate',
-'GET /rent/jpaginate': 'RentController.jpaginate',
-'GET /rent/jjpaginate': 'RentController.jjpaginate',
-'POST /rent/paginate': 'RentController.paginate',
+  '/': 'RentController.home',
 
-// 'GET /rent/my' : 'RentController.my',
-// 'POST /rent/my' : 'RentController.my',
+  'GET /rent/home': 'RentController.home',
+  'GET /rent/jhome': 'RentController.jhome',
+  'GET /rent/jestate': 'RentController.jestate',
 
-// 'GET /rent/occupants' : 'RentController.occupants',
-// 'POST /rent/occupants' : 'RentController.occupants',
+  'GET /user/login': 'UserController.login',
+  'POST /user/login': 'UserController.login',
+  'POST /user/logout': 'UserController.logout',
 
+  'POST /user/add': 'UserController.add',
 
-'GET /rent/details/:id': 'RentController.details',
-
-'/' : 'RentController.home',
-
-'GET /rent/home': 'RentController.home',
-'GET /rent/jhome': 'RentController.jhome',
-'GET /rent/jestate': 'RentController.jestate',
-
-'GET /user/login': 'UserController.login',
-'POST /user/login': 'UserController.login',
-'POST /user/logout': 'UserController.logout',
-
-// 'GET /user/add': 'UserController.add',
-'POST /user/add': 'UserController.add',
-
-'GET /rent/:id/rentby': 'RentController.populate',
-'GET /user/:id/renting': 'UserController.populate',
-'POST /user/:id/renting/add/:fk': 'UserController.add',
-'GET /user/:id/renting/add/:fk': 'UserController.add',
-// 'GET /user/:id/renting/remove/:fk': 'UserController.remove',
-'POST /user/:id/renting/remove/:fk': 'UserController.remove',
+  'GET /rent/:id/rentby': 'RentController.populate',
+  'GET /user/:id/renting': 'UserController.populate',
+  'POST /user/:id/renting/add/:fk': 'UserController.add',
+  'GET /user/:id/renting/add/:fk': 'UserController.add',
+  'POST /user/:id/renting/remove/:fk': 'UserController.remove',
 };
